3965a44877e8c8b5a41ff60e04f49085057a6097
susonglin <long1025>
2018-04-10 11:12:30 +0800

    删除notice类报错处

--- src/main/java/com/muheda/notice/kafka/Listener.java
+++ src/main/java/com/muheda/notice/kafka/Listener.java
@@ -1,11 +1,8 @@
 package com.muheda.notice.kafka;
 
-import com.muheda.notice.entity.Notice;
-import com.muheda.notice.service.NoticeService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -17,8 +14,6 @@ import org.springframework.stereotype.Component;
 @Component("listener")
 public class Listener {
 
-    @Autowired
-    private NoticeService noticeService;
 
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -33,36 +28,5 @@ public class Listener {
     public void listen2(ConsumerRecord<?, ?> record) {
         logger.info("kafka2的key: " + record.key());
         logger.info("kafka2的value: " + record.value().toString());
-    }
-
-    /**
-     * @Descripton: 发送通知
-     * @Author: Sorin
-     * @param record
-     * @Date: 2018/3/26
-     */
-    @KafkaListener(topics = {"notice_send"})
-    public void noticeSend(ConsumerRecord<?, ?> record){
-        Notice notice = (Notice)record.value();
-        // 发送通知
-        noticeService.save(notice);
-    }
-
-    /**
-     * @Descripton: 修改通知内容
-     * @Author: Sorin
-     * @param record
-     * @Date: 2018/3/26
-     */
-    @KafkaListener(topics = {"notice_update"})
-    public void noticeUpdate(ConsumerRecord<?, ?> record){
-        try {
-            Notice notice = (Notice)record.value();
-            // 修改通知内容
-            noticeService.update(notice);
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.error("修改通知内容失败！");
-        }
     }
 }
